c197a5ed6017282ccfb6fc4c6949d9c64d217ab1
Michael Zinsmaier <michael.zinsmaier@gmx.de>
2012-09-20 16:25:00 +0200

    added the selection demos from jfreechart-select

---
+++ org/jfree/expdemo/SelectionDemo1.java
@@ -0,0 +1,227 @@
+/* -------------------
+ * SelectionDemo1.java
+ * -------------------
+ * (C) Copyright 2009, by Object Refinery Limited.
+ *
+ */
+
+package org.jfree.expdemo;
+
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.panel.selectionhandler.FreeRegionSelectionHandler;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.general.DatasetChangeEvent;
+import org.jfree.data.general.DatasetChangeListener;
+import org.jfree.data.time.Month;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.NumberCellRenderer;
+import org.jfree.ui.RefineryUtilities;
+
+public class SelectionDemo1 extends ApplicationFrame
+        implements DatasetChangeListener {
+
+    JTable table;
+
+    DefaultTableModel model;
+
+    TimeSeriesCollection dataset;
+
+    /**
+     * A demonstration application showing how to create a simple time series
+     * chart.  This example uses monthly data.
+     *
+     * @param title  the frame title.
+     */
+    public SelectionDemo1(String title) {
+        super(title);
+        ChartPanel chartPanel = (ChartPanel) createDemoPanel();
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+
+        JFreeChart chart = chartPanel.getChart();
+        XYPlot plot = (XYPlot) chart.getPlot();
+        this.dataset = (TimeSeriesCollection) plot.getDataset();
+        this.dataset.addChangeListener(this);
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        split.add(chartPanel);
+
+        this.model = new DefaultTableModel(new String[] {"Series:", "Item:", "Period:", "Value:"}, 0);
+        this.table = new JTable(this.model);
+        TableColumnModel tcm = this.table.getColumnModel();
+        tcm.getColumn(3).setCellRenderer(new NumberCellRenderer());
+        JPanel p = new JPanel(new BorderLayout());
+        JScrollPane scroller = new JScrollPane(this.table);
+        p.add(scroller);
+        p.setBorder(BorderFactory.createCompoundBorder(
+                new TitledBorder("Selected Items: "),
+                new EmptyBorder(4, 4, 4, 4)));
+        split.add(p);
+        setContentPane(split);
+    }
+
+    /**
+     * The dataset changed, so we announce that the table model changed
+     * too (even if maybe it didn't - demo shortcut).
+     *
+     * @param event  ignored.
+     */
+    public void datasetChanged(DatasetChangeEvent event) {
+        while (this.model.getRowCount() > 0) {
+            this.model.removeRow(0);
+        }
+        int seriesCount = this.dataset.getSeriesCount();
+        for (int s = 0; s < seriesCount; s++) {
+            int itemCount = this.dataset.getItemCount(s);
+            for (int i = 0; i < itemCount; i++) {
+//IMPL NEEDED            	
+//                if (!this.dataset.isSelected(s, i)) {
+//                    continue;
+//                }
+//                Comparable seriesKey = this.dataset.getSeriesKey(s);
+//                RegularTimePeriod p = this.dataset.getSeries(s).getTimePeriod(i);
+//                Number value = this.dataset.getY(s, i);
+//                this.model.addRow(new Object[] { seriesKey, new Integer(i), p,
+//                    value});
+            }
+        }
+    }
+
+    /**
+     * Creates a chart.
+     *
+     * @param dataset  a dataset.
+     *
+     * @return A chart.
+     */
+    private static JFreeChart createChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+            "Stock Prices",  // title
+            "Date",             // x-axis label
+            "Price Per Unit",   // y-axis label
+            dataset,            // data
+            true,               // create legend?
+            true,               // generate tooltips?
+            false               // generate URLs?
+        );
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setDomainPannable(true);
+        plot.setRangePannable(true);
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+        XYLineAndShapeRenderer r = (XYLineAndShapeRenderer) plot.getRenderer();
+        r.setBaseShapesVisible(true);
+        r.setBaseShapesFilled(true);
+        r.setUseFillPaint(true);
+        r.setSeriesFillPaint(0, r.lookupSeriesPaint(0));
+        r.setSeriesFillPaint(1, r.lookupSeriesPaint(1));
+        return chart;
+
+    }
+
+    /**
+     * Creates a dataset, consisting of two series of monthly data.
+     *
+     * @return The dataset.
+     */
+    private static XYDataset createDataset() {
+
+        TimeSeries s1 = new TimeSeries("S1");
+        s1.add(new Month(1, 2009), 181.8);
+        s1.add(new Month(2, 2009), 167.3);
+        s1.add(new Month(3, 2009), 153.8);
+        s1.add(new Month(4, 2009), 167.6);
+        s1.add(new Month(5, 2009), 158.8);
+        s1.add(new Month(6, 2009), 148.3);
+        s1.add(new Month(7, 2009), 153.9);
+        s1.add(new Month(8, 2009), 142.7);
+        s1.add(new Month(9, 2009), 123.2);
+        s1.add(new Month(10, 2009), 131.8);
+        s1.add(new Month(11, 2009), 139.6);
+        s1.add(new Month(12, 2009), 142.9);
+        s1.add(new Month(1, 2010), 138.7);
+        s1.add(new Month(2, 2010), 137.3);
+        s1.add(new Month(3, 2010), 143.9);
+        s1.add(new Month(4, 2010), 139.8);
+        s1.add(new Month(5, 2010), 137.0);
+        s1.add(new Month(6, 2010), 132.8);
+
+        TimeSeries s2 = new TimeSeries("S2");
+        s2.add(new Month(1, 2009), 129.6);
+        s2.add(new Month(2, 2009), 123.2);
+        s2.add(new Month(3, 2009), 117.2);
+        s2.add(new Month(4, 2009), 124.1);
+        s2.add(new Month(5, 2009), 122.6);
+        s2.add(new Month(6, 2009), 119.2);
+        s2.add(new Month(7, 2009), 116.5);
+        s2.add(new Month(8, 2009), 112.7);
+        s2.add(new Month(9, 2009), 101.5);
+        s2.add(new Month(10, 2009), 106.1);
+        s2.add(new Month(11, 2009), 110.3);
+        s2.add(new Month(12, 2009), 111.7);
+        s2.add(new Month(1, 2010), 111.0);
+        s2.add(new Month(2, 2010), 109.6);
+        s2.add(new Month(3, 2010), 113.2);
+        s2.add(new Month(4, 2010), 111.6);
+        s2.add(new Month(5, 2010), 108.8);
+        s2.add(new Month(6, 2010), 101.6);
+
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(s1);
+        dataset.addSeries(s2);
+
+        return dataset;
+
+    }
+
+    /**
+     * Creates a panel for the demo (used by SuperDemo.java).
+     *
+     * @return A panel.
+     */
+    public static JPanel createDemoPanel() {
+        JFreeChart chart = createChart(createDataset());
+        ChartPanel panel = new ChartPanel(chart);
+        panel.setFillZoomRectangle(true);
+        panel.setMouseWheelEnabled(true);
+
+
+
+        FreeRegionSelectionHandler selectionHandler = new FreeRegionSelectionHandler();
+        //selectionHandler.setModifier(InputEvent.SHIFT_MASK);
+        panel.addMouseHandler(selectionHandler);
+        return panel;
+    }
+
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+    public static void main(String[] args) {
+        SelectionDemo1 demo = new SelectionDemo1(
+                "JFreeChart: SelectionDemo1");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+    }
+
+}
---
+++ org/jfree/expdemo/SelectionDemo3.java
@@ -0,0 +1,204 @@
+/* -------------------
+ * SelectionDemo3.java
+ * -------------------
+ * (C) Copyright 2009, by Object Refinery Limited.
+ *
+ */
+
+package org.jfree.expdemo;
+
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.Random;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.panel.selectionhandler.CircularRegionSelectionHandler;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYDotRenderer;
+import org.jfree.data.general.DatasetChangeEvent;
+import org.jfree.data.general.DatasetChangeListener;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.NumberCellRenderer;
+import org.jfree.ui.RefineryUtilities;
+
+/**
+ * A demo scatter plot.
+ */
+public class SelectionDemo3 extends ApplicationFrame implements DatasetChangeListener {
+
+    JTable table;
+
+    DefaultTableModel model;
+
+    XYSeriesCollection dataset;
+
+    /**
+     * A demonstration application showing a scatter plot.
+     *
+     * @param title  the frame title.
+     */
+    public SelectionDemo3(String title) {
+        super(title);
+        ChartPanel chartPanel = (ChartPanel) createDemoPanel();
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+
+        JFreeChart chart = chartPanel.getChart();
+        XYPlot plot = (XYPlot) chart.getPlot();
+        this.dataset = (XYSeriesCollection) plot.getDataset();
+        this.dataset.addChangeListener(this);
+        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        split.add(chartPanel);
+
+        this.model = new DefaultTableModel(new String[] {"Series:", "Item:", "X:", "Y:"}, 0);
+        this.table = new JTable(this.model);
+        TableColumnModel tcm = this.table.getColumnModel();
+        tcm.getColumn(2).setCellRenderer(new NumberCellRenderer());
+        tcm.getColumn(3).setCellRenderer(new NumberCellRenderer());
+        JPanel p = new JPanel(new BorderLayout());
+        JScrollPane scroller = new JScrollPane(this.table);
+        p.add(scroller);
+        p.setBorder(BorderFactory.createCompoundBorder(
+                new TitledBorder("Selected Items: "),
+                new EmptyBorder(4, 4, 4, 4)));
+        split.add(p);
+        setContentPane(split);
+    }
+
+    private static JFreeChart createChart(XYDataset dataset) {
+        JFreeChart chart = ChartFactory.createScatterPlot("SelectionDemo3",
+                "X", "Y", dataset, PlotOrientation.VERTICAL, true, true, false);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setNoDataMessage("NO DATA");
+
+        plot.setDomainPannable(true);
+        plot.setRangePannable(true);
+        plot.setDomainZeroBaselineVisible(true);
+        plot.setRangeZeroBaselineVisible(true);
+
+        plot.setDomainGridlineStroke(new BasicStroke(0.0f));
+        plot.setRangeGridlineStroke(new BasicStroke(0.0f));
+
+        plot.setDomainMinorGridlinesVisible(true);
+        plot.setRangeMinorGridlinesVisible(true);
+
+        //XYItemRenderer r = plot.getRenderer();
+        XYDotRenderer r = new XYDotRenderer();
+       r.setDotHeight(2);
+        r.setDotWidth(2);
+        r.setSeriesPaint(0, Color.blue);
+        r.setSeriesPaint(1, Color.green);
+        r.setSeriesPaint(2, Color.yellow);
+        r.setSeriesPaint(3, Color.orange);
+        plot.setRenderer(r);
+        NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
+        domainAxis.setAutoRangeIncludesZero(false);
+
+        domainAxis.setTickMarkInsideLength(2.0f);
+        domainAxis.setTickMarkOutsideLength(2.0f);
+
+        domainAxis.setMinorTickCount(2);
+        domainAxis.setMinorTickMarksVisible(true);
+
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setTickMarkInsideLength(2.0f);
+        rangeAxis.setTickMarkOutsideLength(2.0f);
+        rangeAxis.setMinorTickCount(2);
+        rangeAxis.setMinorTickMarksVisible(true);
+        return chart;
+    }
+
+     /**
+     * The dataset changed, so we announce that the table model changed
+     * too (even if maybe it didn't - demo shortcut).
+     *
+     * @param event  ignored.
+     */
+    public void datasetChanged(DatasetChangeEvent event) {
+        while (this.model.getRowCount() > 0) {
+            this.model.removeRow(0);
+        }
+        int seriesCount = this.dataset.getSeriesCount();
+        for (int s = 0; s < seriesCount; s++) {
+            int itemCount = this.dataset.getItemCount(s);
+            for (int i = 0; i < itemCount; i++) {
+//IMPL NEEDED            	
+//                if (!this.dataset.isSelected(s, i)) {
+//                    continue;
+//                }
+//                Comparable seriesKey = this.dataset.getSeriesKey(s);
+//                Number x = this.dataset.getX(s, i);
+//                Number y = this.dataset.getY(s, i);
+//                this.model.addRow(new Object[] { seriesKey, new Integer(i), x,
+//                    y});
+            }
+        }
+    }
+
+    public static XYDataset createDataset() {
+        String[] names = {"JFreeChart", "Eastwood", "Orson", "JCommon"};
+        Random rgen = new Random();
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        for (int s = 0; s < 4; s++) {
+            XYSeries series = new XYSeries(names[s]);
+            for (int i = 0; i < 5000; i++) {
+                double x = rgen.nextGaussian() * 200;
+                double y = rgen.nextGaussian() * 200;
+                series.add(x, y);
+            }
+            dataset.addSeries(series);
+        }
+        return dataset;
+    }
+
+    /**
+     * Creates a panel for the demo (used by SuperDemo.java).
+     *
+     * @return A panel.
+     */
+    public static JPanel createDemoPanel() {
+        JFreeChart chart = createChart(createDataset());
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setMouseWheelEnabled(true);
+
+        chartPanel.removeMouseHandler(chartPanel.getZoomHandler());
+
+        CircularRegionSelectionHandler selectionHandler = new CircularRegionSelectionHandler();
+        //selectionHandler.setModifier(InputEvent.SHIFT_MASK);
+        chartPanel.addMouseHandler(selectionHandler);
+        return chartPanel;
+    }
+
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+    public static void main(String[] args) {
+        SelectionDemo3 demo = new SelectionDemo3(
+                "JFreeChart: SelectionDemo3.java");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+    }
+
+}
+
