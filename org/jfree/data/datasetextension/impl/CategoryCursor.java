fb5bd0d752d4988895892cbb3552f785c37cc822
Michael Zinsmaier <michael.zinsmaier@gmx.de>
2012-09-25 10:21:10 +0200

    implemented category and pie cursor

--- org/jfree/data/datasetextension/impl/CategoryCursor.java
+++ org/jfree/data/datasetextension/impl/CategoryCursor.java
@@ -4,4 +4,20 @@ import org.jfree.data.datasetextension.DatasetCursor;
 
 public class CategoryCursor implements DatasetCursor {
 
+	private Comparable rowKey;
+	private Comparable columnKey;
+	
+	public void setPosition(Comparable rowKey, Comparable columnKey) {
+		this.rowKey = rowKey;
+		this.columnKey = columnKey;
+	}
+	
+	public Comparable getRowKey() {
+		return this.rowKey;
+	}
+	
+	public Comparable getColumnKey() {
+		return this.columnKey;
+	}
+	
 }
--- org/jfree/data/datasetextension/impl/PieCursor.java
+++ org/jfree/data/datasetextension/impl/PieCursor.java
@@ -4,4 +4,14 @@ import org.jfree.data.datasetextension.DatasetCursor;
 
 public class PieCursor implements DatasetCursor {
 
+	private Comparable key;
+	
+	public void setPosition(Comparable key) {
+		this.key = key;
+	}
+	
+	public Comparable getKey() {
+		return this.key;
+	}
+		
 }
